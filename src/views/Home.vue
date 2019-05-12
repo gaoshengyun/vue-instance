c24647835da35fc4a6956335fd350d274e605d38
gaoshengyun <18616242909@163.com>
2019-05-12 15:48:48 +0800

    修改导航

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{msg}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      msg:'平时vue项目中遇到的功能'
+    }
+  },
 }
 </script>
